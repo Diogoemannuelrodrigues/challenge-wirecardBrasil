637712cacc7d265efd502bb139a0d2fa8b28a324
Diogo Emannuel <diogoemannuel.rodrigues1@gmail.com>
2024-05-23 11:07:01 -0300

    Atualizando .gitignore para ignorar arquivos desnecessários

---
+++ backend/wirecardBrasil/src/main/java/br/com/github/wirecardBrasil/WirecardBrasilApplication.java
@@ -0,0 +1,13 @@
+package br.com.github.wirecardBrasil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirecardBrasilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WirecardBrasilApplication.class, args);
+	}
+
+}
---
+++ backend/wirecardBrasil/src/test/java/br/com/github/wirecardBrasil/WirecardBrasilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.github.wirecardBrasil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirecardBrasilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
